a7bf5b25ac9af7dc162e9b31f78da3b0abd99ca0
Javier Garcia <jgarciacot@wiley.com>
2016-09-21 10:13:45 +0100

    Refactored application entry point for scraping the grocery site

--- src/main/java/uk/co/sainsburys/exercise/Main.java
+++ src/main/java/uk/co/sainsburys/exercise/Main.java
@@ -1,11 +1,24 @@
 package uk.co.sainsburys.exercise;
 
+import uk.co.sainsburys.exercise.calculator.Calculator;
+import uk.co.sainsburys.exercise.output.impl.FileWriterOutput;
+import uk.co.sainsburys.exercise.page.WebPage;
+import uk.co.sainsburys.exercise.parser.impl.RipeFruitsParser;
+import uk.co.sainsburys.exercise.properties.PropertiesReader;
+import uk.co.sainsburys.exercise.scraper.WebScraper;
+
+import java.io.IOException;
+
 /**
  * Setting an Application's Entry Point
  */
-public class Main {
+class Main {
 
-    public static void main(String[] args) {
-        System.out.println("hello, world!");
+    public static void main(String[] args) throws IOException {
+        PropertiesReader propertiesReader = new PropertiesReader("/application.properties");
+        final WebScraper webScraper = new WebScraper(propertiesReader, new RipeFruitsParser(), new WebPage(), new Calculator(), new FileWriterOutput());
+        webScraper.scrape();
+        System.out.println("Sainsbury's web scraper finished");
+        System.out.println("Please find the results at " + propertiesReader.get("sainsburys.output.file.name") + " file");
     }
 }
