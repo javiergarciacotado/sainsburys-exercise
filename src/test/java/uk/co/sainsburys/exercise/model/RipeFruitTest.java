e0c76e79436bd248e3ecd95eab36281250430e2f
Javier Garcia <jgarciacot@wiley.com>
2016-09-21 10:11:57 +0100

    Refactored test for POJO

--- src/test/java/uk/co/sainsburys/exercise/model/RipeFruitTest.java
+++ src/test/java/uk/co/sainsburys/exercise/model/RipeFruitTest.java
@@ -2,16 +2,16 @@ package uk.co.sainsburys.exercise.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class RipeFruitTest {
 
     @Test
-    public void givenARipeFruitValidateRipeFruit() {
-        RipeFruit ripeFruit = new RipeFruit("title", "description", 1.5F, "100kb");
+    public void givenARipeFruitShouldReturnCorrectValues() {
+        RipeFruit ripeFruit = new RipeFruit("title", "description", 1.5D, "100kb");
         assertEquals("title", ripeFruit.getTitle());
         assertEquals("description", ripeFruit.getDescription());
-        assertEquals(1.5F, ripeFruit.getUnitPrice(), 0.0F);
+        assertEquals(1.5D, ripeFruit.getUnitPrice(), 0.0D);
         assertEquals("100kb", ripeFruit.getSize());
     }
 
