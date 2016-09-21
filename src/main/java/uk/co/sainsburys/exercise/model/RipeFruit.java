08c8d9e14986796d990750c74efcc48208081eb5
Javier Garcia <jgarciacot@wiley.com>
2016-09-21 10:02:13 +0100

    Refactored Javadoc documentation and implemented equals for comparisons

--- src/main/java/uk/co/sainsburys/exercise/model/RipeFruit.java
+++ src/main/java/uk/co/sainsburys/exercise/model/RipeFruit.java
@@ -7,7 +7,7 @@ public class RipeFruit {
 
     private final String title;
     private final String description;
-    private final float unitPrice;
+    private final double unitPrice;
     private final String size;
 
     /**
@@ -17,11 +17,35 @@ public class RipeFruit {
      * @param unitPrice Product unit price
      * @param size Product page link size
      */
-    public RipeFruit(String title, String description, float unitPrice, String size) {
+    public RipeFruit(String title, String description, double unitPrice, String size) {
         this.title = title;
         this.description = description;
         this.unitPrice = unitPrice;
         this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RipeFruit ripeFruit = (RipeFruit) o;
+
+        if (Double.compare(ripeFruit.unitPrice, unitPrice) != 0) return false;
+        if (!title.equals(ripeFruit.title)) return false;
+        if (!description.equals(ripeFruit.description)) return false;
+        return size.equals(ripeFruit.size);
+
+    }
+
+    @Override
+    public String toString() {
+        return "RipeFruit{" +
+                "description='" + description + '\'' +
+                ", title='" + title + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", size='" + size + '\'' +
+                '}';
     }
 
     public String getDescription() {
@@ -36,17 +60,8 @@ public class RipeFruit {
         return title;
     }
 
-    public float getUnitPrice() {
+    public double getUnitPrice() {
         return unitPrice;
     }
 
-    @Override
-    public String toString() {
-        return "RipeFruit{" +
-                "description='" + description + '\'' +
-                ", title='" + title + '\'' +
-                ", unitPrice=" + unitPrice +
-                ", size='" + size + '\'' +
-                '}';
-    }
 }
